c191d3b240f29ead750615919b759a0b1a5865be
Florent CHRETIEN <flopubs@gmail.com>
2019-05-12 20:58:27 +0200

    add submodule ArduinoJson Programm reach out to both HUE and print the Status of all lamps on the network

--- include/config.h
+++ include/config.h
@@ -1,6 +1,8 @@
 const char* ssid = "der-palmenhain";
 const char* password = "bonjourstuttgart";
 
+const int NB_HUE_DEVICES = 2;
+
 const char* ip_hue = "192.168.1.2";
 const char* ip_pi = "192.168.1.4";
 const char* dev_id_hue = "fRkcEAkfYx6CkGjl0FDmTzttW-sD9LPFyX9syBRj";
--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,16 @@
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 
+#include <ArduinoJson.h>
+
 #include "config.h"
 #include "requests.h"
 
+int address_check = 0;
 
 void setup() {
+
+
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println();
@@ -28,12 +33,17 @@ void setup() {
 void loop() {
   if(WiFi.status()== WL_CONNECTED){
     // We are connected to the Wifi
-
     HTTPClient http;
 
     String address_hue = String("http://" + String(ip_hue) + "/api/" + String(dev_id_hue) + "/lights");
     String address_pi = String("http://" + String(ip_pi) + "/api/" + String(dev_id_pi) + "/lights");
-    int httpResponseCode = get_request(&http, address_pi);
+
+    String my_addresses[NB_HUE_DEVICES];
+    my_addresses[0] = address_hue;
+    my_addresses[1] = address_pi;
+
+    int httpResponseCode = get_request(&http, my_addresses[address_check]);
+    address_check = (address_check + 1) % NB_HUE_DEVICES;
 
    // String data = "{\"on\":true}";
    // int httpResponseCode = http.sendRequest("PUT", data.c_str());
@@ -42,8 +52,36 @@ void loop() {
 
       String response = http.getString();
 
-      Serial.println(httpResponseCode);
-      Serial.println(response);
+      // const size_t capacity = 12*JSON_ARRAY_SIZE(2) + 3*JSON_ARRAY_SIZE(3) + 16*JSON_OBJECT_SIZE(2) + 4*JSON_OBJECT_SIZE(3) + 5*JSON_OBJECT_SIZE(4) + 4*JSON_OBJECT_SIZE(5) + 3*JSON_OBJECT_SIZE(11) + 4*JSON_OBJECT_SIZE(13) + 2610;
+      const size_t capacity = response.length() * 1.73;
+      DynamicJsonDocument doc(capacity);
+
+      DeserializationError error = deserializeJson(doc, response);
+      if (error) {
+        Serial.print(F("deserializeJson() failed: "));
+        Serial.println(error.c_str());
+      }
+
+      // JsonObject lamp = doc["4"];
+      // JsonObject lamp_state = lamp["state"];
+      // bool lamp_state_on = lamp_state["on"].as<bool>();
+      // int lamp_state_bri = lamp_state["bri"].as<int>();
+      //
+      // Serial.println(httpResponseCode);
+      // Serial.println(response.length());
+      Serial.println(doc.size());
+
+      for (size_t i = 0; i < doc.size(); i++) {
+        JsonObject lamp = doc[String(i + 1)];
+        JsonObject lamp_state = lamp["state"];
+        bool lamp_state_on = lamp_state["on"].as<bool>();
+        int lamp_state_bri = lamp_state["bri"].as<int>();
+
+        Serial.print("State:");
+        Serial.println(lamp_state_on);
+        Serial.print("Brightness:");
+        Serial.println(lamp_state_bri);
+      }
 
     } else {
 
